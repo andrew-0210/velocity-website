20df06cc4a479aa524ec120da9ff545aea1e17cd
Andrew Monthero <166970322+andrew-0210@users.noreply.github.com>
2024-09-16 13:05:30 +1000

    Update Statistics.tsx

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,3 +1,5 @@
+import Container from "./Container";
+
 const stats = [
   {
     stat: "500K",
@@ -17,29 +19,34 @@ const stats = [
 
 const Statistics = () => {
   return (
-    <section className="flex flex-col gap-5 py-[7.5rem]">
-      <h2 className="text-center text-[1.5rem] font-semibold leading-tight tracking-tighter md:text-[2rem]">
-        User Statistics for E-Wallet: Insights into Platform
-        <br /> Engagement and Growth.
-      </h2>
-      <p className="text-center font-medium tracking-tight text-[#797979]">
-        Explore user data to understand E-wallet usage patterns, engagement
-        levels,and
-        <br /> platform expansion opportunities for informed decisions.
-      </p>
-      <div className="flex flex-col gap-2 md:grid md:grid-cols-12 md:gap-[2rem]">
-        {stats.map((data) => (
-          <div className="col-span-4 rounded-xl border border-[#d8d8d8] px-[2rem] py-[2rem]" key={data.stat}>
-            <p className="mb-[0.5rem] text-center text-[1.75rem] font-semibold tracking-tight">
-              {data.stat}
-            </p>
-            <p className="text-center font-medium tracking-tight text-[#797979]">
-              {data.description}
-            </p>
-          </div>
-        ))}
-      </div>
-    </section>
+    <Container>
+      <section className="flex flex-col gap-5 py-[7.5rem]">
+        <h2 className="text-center text-[1.5rem] font-semibold leading-tight tracking-tighter md:text-[2rem]">
+          User Statistics for E-Wallet: Insights into Platform
+          <br /> Engagement and Growth.
+        </h2>
+        <p className="text-center font-medium tracking-tight text-[#797979]">
+          Explore user data to understand E-wallet usage patterns, engagement
+          levels,and
+          <br /> platform expansion opportunities for informed decisions.
+        </p>
+        <div className="flex flex-col gap-2 md:grid md:grid-cols-12 md:gap-[2rem]">
+          {stats.map((data) => (
+            <div
+              className="col-span-4 rounded-xl border border-[#d8d8d8] px-[2rem] py-[2rem]"
+              key={data.stat}
+            >
+              <p className="mb-[0.5rem] text-center text-[1.75rem] font-semibold tracking-tight">
+                {data.stat}
+              </p>
+              <p className="text-center font-medium tracking-tight text-[#797979]">
+                {data.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Container>
   );
 };
 
